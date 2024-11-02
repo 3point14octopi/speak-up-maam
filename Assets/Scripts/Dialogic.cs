be9dc3e43b3f7b3679c7705887f618011642d651
3point14octopi <72412274+3point14octopi@users.noreply.github.com>
2024-11-02 13:40:42 -0400

    3point scale for most attributes

---
+++ Assets/Scripts/Dialogic.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public enum Scale3Vals
+{
+    LOW = 0,
+    MID = 1,
+    HIGH = 2
+}
+
+
+
+
+
+
+
+public class Dialogic:MonoBehaviour
+{
+    public ThreeScale temperature;
+    public ThreeScale size;
+    public ThreeScale strength;
+
+    public Scale3Vals selectedTemperature;
+    public Scale3Vals selectedSize;
+    public Scale3Vals selectedStrength;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shuffle(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Check();
+        }
+    }
+
+    public void Check()
+    {
+        bool wrong = false;
+
+        wrong = (temperature.CompareAttribute(selectedTemperature))?true:wrong;
+        wrong = (size.CompareAttribute(selectedSize))?true:wrong;
+        wrong = (strength.CompareAttribute(selectedStrength))?true:wrong;
+
+        if (!wrong)
+        {
+            Debug.Log("u dids it");
+            Shuffle();
+        }
+    }
+
+    void Shuffle(bool shuffleAll = false)
+    {
+        temperature.SetValue((Scale3Vals)Random.Range(0, 3));
+        size.SetValue((Scale3Vals)Random.Range(0, 3));
+        strength.SetValue((Scale3Vals)Random.Range(0, 3));
+
+        if (!shuffleAll) return;
+
+        selectedTemperature = (Scale3Vals)Random.Range(0, 3);
+        selectedSize = (Scale3Vals)Random.Range(0, 3);
+        selectedStrength = (Scale3Vals)Random.Range(0, 3);
+    }
+}
---
+++ Assets/Scripts/ThreeScale.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeScale : MonoBehaviour
+{
+    [SerializeField]protected Scale3Vals myVal;
+    bool incorrect = true;
+
+    //change this
+    public List<string> xLowComplain  = new List<string>();
+    public List<string> lowComplain  = new List<string>();
+    public List<string> highComplain = new List<string>();
+    public List<string> xHighComplain = new List<string>();
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetValue(Scale3Vals newVal)
+    {
+        myVal = newVal;
+    }
+
+    public bool CompareAttribute(Scale3Vals compareVal)
+    {
+        incorrect = false;
+        if(myVal == compareVal + 2)
+        {
+            Complain(xLowComplain);
+        }else if(myVal == compareVal + 1)
+        {
+            Complain(lowComplain);
+        }else if(myVal == compareVal - 1)
+        {
+            Complain(highComplain);
+        }else if(myVal == compareVal - 2)
+        {
+            Complain(xHighComplain);
+        }
+
+        return incorrect;
+    }
+
+    public void Complain(List<string> complaintPool)
+    {
+        Debug.Log(complaintPool[Random.Range(0, complaintPool.Count)]);
+        incorrect = true;
+    }
+}
